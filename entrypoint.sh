a49a266b391b7f00d3ba4b6c072e2b6cbfe01d05
Jérémie Parker <hi@pkr.sh>
2023-10-08 19:40:17 +0200

    fix: do not say yes

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,13 +55,13 @@ fi
 
 # Scale the VM before the deploy.
 if [ -n "$INPUT_VM" ]; then
-  flyctl scale --yes --app "$app" vm "$INPUT_VM"
+  flyctl scale --app "$app" vm "$INPUT_VM"
 fi
 if [ -n "$INPUT_MEMORY" ]; then
-  flyctl scale --yes --app "$app" memory "$INPUT_MEMORY"
+  flyctl scale --app "$app" memory "$INPUT_MEMORY"
 fi
 if [ -n "$INPUT_COUNT" ]; then
-  flyctl scale --yes --app "$app" count "$INPUT_COUNT"
+  flyctl scale --app "$app" count "$INPUT_COUNT"
 fi
 
 # Attach postgres cluster to the app if specified.
